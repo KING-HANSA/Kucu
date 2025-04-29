a41b23375bcd4dcdcc16f2d11134f74c72153e1f
KING-HANSA <hansadewmina2008@gmail.com>
2025-04-29 23:23:08 +0530

    Update cred.js

--- js/cred.js
+++ js/cred.js
@@ -1,9 +1,9 @@
-firebaseConfig = {
-	apiKey: "AIzaSyAewQIxSBj71HSEKg1BOVMakeRp13TvKNk",
-	authDomain: "pm-e-avurudu.firebaseapp.com",
-	projectId: "pm-e-avurudu",
-	storageBucket: "pm-e-avurudu.appspot.com",
-	messagingSenderId: "586671365848",
-	appId: "1:586671365848:web:6ea67daf32ddda580501ad",
-	auth: { uid: window.localStorage.getItem('uid') }
+const firebaseConfig = {
+  apiKey: "AIzaSyCDIwUiq7g9NwEBR_kzbTVNOlH7yACveMw",
+  authDomain: "kuku-48534.firebaseapp.com",
+  projectId: "kuku-48534",
+  storageBucket: "kuku-48534.firebasestorage.app",
+  messagingSenderId: "277800721264",
+  appId: "1:277800721264:web:5b79677621e9a60c0bcdb5",
+  measurementId: "G-6M4L4Y3HF1"
 };
